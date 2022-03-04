3d9386f4258d3921f8ec4b664c69b4a1bf6d7856
Arystan <a_r_s_t_a_n@mail.ru>
2022-03-04 16:55:32 +0700

    Transactions tests
    Made tests for checking transactions to contract

--- test/Box.test.js
+++ test/Box.test.js
@@ -20,12 +20,12 @@ describe("SimpleTransaction", function () {
   });
 
   it("If transaction value < 0 should revert", async function () {
-    let options = {
+    let transaction = {
       to: this.contract.address,
       value: await ethers.utils.parseEther("0")
-    }
+    };
 
-    await expect(addr1.sendTransaction(options)).to.be.revertedWith("Transaction value is less than zero");
+    await expect(addr1.sendTransaction(transaction)).to.be.revertedWith("Transaction value is less than zero");
 
     await expect(await this.contract.getTransactionValue(addr1.address)).to.equal(0);
 
@@ -33,4 +33,48 @@ describe("SimpleTransaction", function () {
 
     await expect(await this.contract.getBalance()).to.equal(0);
   });
+
+  it("If transaction correct, balance, transactions mapping and transactors are refreshed", async function() {
+    let transaction = {
+      to: this.contract.address,
+      value: await ethers.utils.parseEther("1")
+    };
+
+    await addr1.sendTransaction(transaction);
+
+    await expect(await this.contract.getTransactionValue(addr1.address)).to.equal(transaction.value);
+
+    let transactors = await this.contract.getTransactors();
+    await expect(transactors[0]).to.equal(addr1.address);
+
+    await expect(await this.contract.getBalance()).to.equal(transaction.value);
+  });
+
+  it("Should not be any duplicate transactor in transactors array", async function() {
+    let transaction = {
+      to: this.contract.address,
+      value: await ethers.utils.parseEther("1")
+    };
+
+    await addr1.sendTransaction(transaction);
+
+    let transactors = await this.contract.getTransactors();
+    await expect(transactors[0]).to.equal(addr1.address);
+    await expect(transactors[1]).to.equal(undefined);
+  });
+
+  it("Different transactions should change totalBalance", async function() {
+    let transaction = {
+      to: this.contract.address,
+      value: await ethers.utils.parseEther("1")
+    };
+
+    await addr1.sendTransaction(transaction);
+    transaction.value = await ethers.utils.parseEther("5");
+    await addr1.sendTransaction(transaction);
+    let sumOfTransactionsValue = await ethers.utils.parseEther("6");
+
+    await expect(await this.contract.getBalance()).to.equal(sumOfTransactionsValue);
+  });
+
 });
